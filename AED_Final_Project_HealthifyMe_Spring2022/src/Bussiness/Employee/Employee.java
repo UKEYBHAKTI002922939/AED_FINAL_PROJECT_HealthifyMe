593225464b6a1d12dcdf778260ea8a4d0ff17fa6
Simrannagpurkar <nagpurkar.s@northeastern.edu>
2022-04-08 17:01:04 -0500

    adding UI for customer

---
+++ AED_Final_Project_HealthifyMe_Spring2022/src/Bussiness/Employee/Employee.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bussiness.Employee;
+
+/**
+ *
+ * @author Simran
+ */
+public class Employee {
+
+    private String name;
+    private int id;
+    private static int count = 1;
+    private Role role;
+    
+}
