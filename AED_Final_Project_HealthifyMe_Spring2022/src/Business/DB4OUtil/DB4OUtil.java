66bfc85ec1d853f7d4175b569b5839783173b5da
UKEYBHAKTI002922939 <ukey.b@northeastern.edu>
2022-04-09 16:56:05 -0400

    adding database db4

--- AED_Final_Project_HealthifyMe_Spring2022/src/Business/DB4OUtil/DB4OUtil.java
+++ AED_Final_Project_HealthifyMe_Spring2022/src/Business/DB4OUtil/DB4OUtil.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Business;
+package Business.DB4OUtil;
 
 /**
  *
- * @author agarw
+ * @author anky
  */
-public class HealthifyMeEcosystem {
+public class DB4OUtil {
     
 }
