5cc2e47945bc96f6604ba5a875a804e26b639ac1
Bhawana Agarwal <agarwal.bh@northeastern.edu>
2022-04-10 18:27:12 -0400

    updated customer java file

--- AED_Final_Project_HealthifyMe_Spring2022/src/Business/Customer/Customer.java
+++ AED_Final_Project_HealthifyMe_Spring2022/src/Business/Customer/Customer.java
@@ -22,7 +22,108 @@ public class Customer {
     private String mobileNumber;
     private String city;
     private Date createDate;
-     
+    
+    public enum Gender{
+        Male("Male"), Female("Female"), Other("Other");
+        private String value;
+        private Gender(String value) {
+            this.value = value;
+        }
+        public String getValue() {
+            return value;
+        }
+    }
+    public Customer() {
+        ++customerId;
+    }
 
+    public static int getCustomerId() {
+        return customerId;
+    }
+
+    public static void setCustomerId(int customerId) {
+        Customer.customerId = customerId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getReenterPassword() {
+        return reenterPassword;
+    }
+
+    public void setReenterPassword(String reenterPassword) {
+        this.reenterPassword = reenterPassword;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
 }
+
 
