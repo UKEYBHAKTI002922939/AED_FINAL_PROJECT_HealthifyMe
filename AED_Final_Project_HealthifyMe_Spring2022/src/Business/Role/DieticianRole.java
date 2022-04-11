fea4e27ff4383f0d23dc0ba0b5cc22c89464a327
Simrannagpurkar <nagpurkar.s@northeastern.edu>
2022-04-11 14:09:29 -0500

    DieticianRolefile added

---
+++ AED_Final_Project_HealthifyMe_Spring2022/src/Business/Role/DieticianRole.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.DieticianOrganization;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import UserInterface.DieticianRole.DieticianWorkAreaJPanel;
+import javax.swing.JPanel;
+/**
+ *
+ * @author Simran
+ */
+public class DieticianRole extends Role  {
+
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network) {  
+    return new DieticianWorkAreaJPanel(userProcessContainer, account, (DieticianOrganization)organization, enterprise, business, network);
+    }
+    
+}
