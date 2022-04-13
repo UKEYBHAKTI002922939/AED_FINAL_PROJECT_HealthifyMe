17e5fa4249ae59032126169dff662f747c8c6143
UKEYBHAKTI002922939 <ukey.b@northeastern.edu>
2022-04-12 20:38:53 -0400

    updating spa role

--- AED_Final_Project_HealthifyMe_Spring2022/src/Business/Role/SpaRole.java
+++ AED_Final_Project_HealthifyMe_Spring2022/src/Business/Role/SpaRole.java
@@ -3,25 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Business.Role;
-
 import Business.EcoSystem;
 import Business.Enterprise.Enterprise;
 import Business.Network.Network;
-import Business.Organization.MeditationOrganization;
+import Business.Organization.DoctorOrganization;
 import Business.Organization.Organization;
+import Business.Organization.SpaOrganization;
 import Business.UserAccount.UserAccount;
-import UserInterface.HouseCleaningRole.HouseCleanWorkAreaJPanel;
+import UserInterface.SpaRole.SpaWorkAreaJPanel;
 import javax.swing.JPanel;
 
 /**
  *
  * @author anky
  */
-public class MeditationInstructor extends Role{
-    
+public class SpaRole extends Role {
+
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network) {  
-    return new HouseCleanWorkAreaJPanel(userProcessContainer, account, (MeditationOrganization)organization, enterprise, business, network);
+    return new SpaWorkAreaJPanel(userProcessContainer, account, (SpaOrganization)organization, enterprise, business, network);
     }    
-
+   
 }
