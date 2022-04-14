2f7c5081738997bfecdcd1e8c2b8f862fc4af44c
UKEYBHAKTI002922939 <ukey.b@northeastern.edu>
2022-04-13 20:56:10 -0400

    adding workqueue request for doctor

---
+++ AED_Final_Project_HealthifyMe_Spring2022/src/Business/WorkQueue/DoctorWorkRequest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author anky
+ */
+public class DoctorWorkRequest extends WorkRequest{
+    private String DoctorAppointment;
+
+    public String getDoctorAppointment() {
+        return DoctorAppointment;
+    }
+
+    public void setDoctorAppointment(String DoctorAppointment) {
+        this.DoctorAppointment = DoctorAppointment;
+    }
+
+    
+}
+
