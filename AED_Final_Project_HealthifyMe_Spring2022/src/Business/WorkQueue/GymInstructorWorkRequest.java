cf8ef337452a1a8e3e3c3cf0edeb40111faafaed
Simrannagpurkar <nagpurkar.s@northeastern.edu>
2022-04-14 19:14:38 -0500

    adding GymInstructorWorkRequest file

---
+++ AED_Final_Project_HealthifyMe_Spring2022/src/Business/WorkQueue/GymInstructorWorkRequest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author Simran
+ */
+public class GymInstructorWorkRequest extends WorkRequest {
+    
+    private String GymInstructorAppointment;
+
+    
+    public String getGymInstructorAppointment() {
+        return GymInstructorAppointment;
+    }
+
+    public void setGymInstructorAppointment(String GymInstructorAppointment) {
+        this.GymInstructorAppointment = GymInstructorAppointment;
+    }
+
+    
+    
+    
+}
