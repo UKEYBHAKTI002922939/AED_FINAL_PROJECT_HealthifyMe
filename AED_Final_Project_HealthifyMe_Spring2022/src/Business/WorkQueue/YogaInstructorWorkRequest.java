b33ee2fa5a08d8b9545fa75f915d2325e7ae8361
Bhawana Agarwal <agarwal.bh@northeastern.edu>
2022-04-21 17:36:05 -0500

    adding yoga instructor work request

---
+++ AED_Final_Project_HealthifyMe_Spring2022/src/Business/WorkQueue/YogaInstructorWorkRequest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author agarw
+ */
+public class YogaInstructorWorkRequest extends WorkRequest {
+    
+    private String YogaInstructorAppointment;
+
+    public String getYogaInstructorAppointment() {
+        return YogaInstructorAppointment;
+    }
+
+    public void setYogaInstructorAppointment(String YogaInstructorAppointment) {
+        this.YogaInstructorAppointment = YogaInstructorAppointment;
+    }  
+    
+    
+}
+
